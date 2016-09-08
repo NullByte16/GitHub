e0079375a09fa840db8dfd2d0d820e486c4a369f
Ezra Gubbay <Ezra Gubbay@DESKTOP-V9CUJ99>
2016-09-08 17:38:06 +0300

    cgsdgd

---
+++ Summer Project/src/Robot/TestRobot.java
@@ -0,0 +1,11 @@
+package Robot;
+
+import java.util.*;
+
+public class TestRobot {
+
+	public static void main(String[] args){
+		RobotsGame game = new RobotsGame();
+		System.out.println(game.game());
+	}
+}
